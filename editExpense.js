133838e7ef4f927d081bb3781b4eaeaeee8ea591
KatarzynaWojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-10-06 00:27:35 +0200

    Created all edit expense

--- editExpense.js
+++ editExpense.js
@@ -1,8 +1,9 @@
 var url = window.location.href;
 var arrayUrl = url.split("?id=");
 var id = arrayUrl[1];
-var baseLink = "http://localhost:8080/expense/details/";
-function fill(expense){
+var baseLink = "http://localhost:8080/expense/details/" + id;
+
+function fill(expense) {
     $('#id-edit').val(expense.id);
     $('#nowy-wydatek-nazwa-input').val(expense.nazwa);
     $('#nowy-wydatek-kategoria-input').val(expense.kategoria);
@@ -11,11 +12,35 @@ function fill(expense){
 }
 
 $.ajax({
-    url: baseLink + id,
+    url: baseLink,
     success: fill,
     error: function (e) {
         console.log(e);
     }
 });
 
-function 
+function saveFunction() {
+    var id = $('#id-edit').val();
+    var newNazwa = $('#nowy-wydatek-nazwa-input').val();
+    var newKategoria = $('#nowy-wydatek-kategoria-input').val();
+    var newCena = $('#nowy-wydatek-cena-input').val();
+    var newStatus = $('#status-select').val();
+    var baseLink = "http://localhost:8080/expense/edit/" + id;
+    var saveFunctionJson = '{"id":"' + id + '","nazwa":"' + newNazwa + '","kategoria":"' + newKategoria + '","cena":' + newCena + ',"status":"' + newStatus + '"}';
+    console.log(saveFunctionJson);
+
+    $.ajax({
+        type: "PUT",
+        url: baseLink,
+        data: saveFunctionJson,
+        contentType: "application/json",
+        success: function () { window.location.pathname = "expense.html" },
+        error: function (e) {
+            $('#edit-expense-error-alert').show();
+            console.log(e);
+        }
+
+    })
+}
+
+$('#save-button').click(saveFunction);
--- expense.js
+++ expense.js
@@ -1,5 +1,6 @@
 var table;
 
+
 $(document).ready(function () {
     table = $('#expenses-table').DataTable({
         dom: 'Bfrtip',
@@ -7,88 +8,109 @@ $(document).ready(function () {
             'copy', 'csv', 'excel', 'pdf',
         ]
     });
+
+
+    $('#expenses-table tbody').on('click', 'tr', function () {
+        if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+        }
+        else {
+            table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
+    });
+
+    $('#edit-button').click(function () {
+        var selectedRow = table.rows('.selected').data()[0];
+        if (selectedRow === undefined) {
+            $('#expense-error-alert').show();
+        } else {
+            var id = selectedRow[0];
+            window.location.href = window.location.origin + "/edit-expense?id=" + encodeURIComponent(id);
+        }
+    });
+
+
+
+    function clearFunction() {
+        table.clear();
+    }
+
+    function addExpensesToTableFunction(expenseList) {
+        console.log(expenseList);
+        clearFunction();
+
+        for (i = 0; i < expenseList.length; i++) {
+            var expense = expenseList[i];
+            table.row.add([expense.id, expense.nazwa, expense.kategoria, expense.cena, expense.status]).draw();
+        }
+
+        clearInputSearchFunction();
+    }
+
+    function searchFunction() {
+        var baseLink = "http://localhost:8080/expense/search";
+        var whereParts = [];
+        var statusParametr = $('#status-select').val();
+        var kategoriaParametr = $('#kategoria-input').val();
+        var nazwaParametr = $('#nazwa-input').val();
+
+        if (statusParametr) {
+            whereParts.push("status=" + statusParametr);
+        }
+
+        if (kategoriaParametr) {
+            whereParts.push("kategoria=" + kategoriaParametr);
+        }
+
+        if (nazwaParametr) {
+            whereParts.push("nazwa=" + nazwaParametr);
+        }
+
+        if (whereParts.length > 0) {
+            baseLink += "?" + whereParts.join('&');
+        }
+        console.log(baseLink);
+
+        $.ajax({
+            url: baseLink,
+            success: addExpensesToTableFunction,
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    }
+
+    function clearInputSearchFunction() {
+        var clearKategoriaInput = $('#kategoria-input');
+        var clearNazwaInput = $('#nazwa-input');
+        var clearStatusSelect = $('#status-select');
+        clearKategoriaInput.val("");
+        clearNazwaInput.val("");
+        clearStatusSelect.val("");
+    }
+
+
+    $('#delete-button').click(function () {
+        var baseLink = "http://localhost:8080/expense/delete/";
+        var selectedRow = table.rows('.selected').data()[0];
+        if (selectedRow === undefined) {
+            $('#expense-error-alert').show();
+        } else {
+            var id = selectedRow[0];
+            $.ajax({
+                url: baseLink + id,
+                type: 'DELETE',
+                success: searchFunction,
+                error: function (e) {
+                    console.log(e);
+                }
+            });
+        }
+    });
+
+
+    $('#search-button').click(searchFunction);
+    $('#clear-button').click(clearFunction);
+    searchFunction();
 })
-
-
-function clearFunction() {
-    table.clear();
-}
-
-function addExpensesToTableFunction(expenseList) {
-    console.log(expenseList);
-    clearFunction();
-
-    for (i = 0; i < expenseList.length; i++) {
-        var expense = expenseList[i];
-        table.row.add([expense.id, expense.nazwa, expense.kategoria, expense.cena, expense.status]).draw();
-    }
-
-    clearInputSearchFunction();
-}
-
-function searchFunction() {
-    var baseLink = "http://localhost:8080/expense/search";
-    var whereParts = [];
-    var statusParametr = $('#status-select').val();
-    var kategoriaParametr = $('#kategoria-input').val();
-    var nazwaParametr = $('#nazwa-input').val();
-
-    if (statusParametr) {
-        whereParts.push("status=" + statusParametr);
-    }
-
-    if (kategoriaParametr) {
-        whereParts.push("kategoria=" + kategoriaParametr);
-    }
-
-    if (nazwaParametr) {
-        whereParts.push("nazwa=" + nazwaParametr);
-    }
-
-    if (whereParts.length > 0) {
-        baseLink += "?" + whereParts.join('&');
-    }
-    console.log(baseLink);
-
-    $.ajax({
-        url: baseLink,
-        success: addExpensesToTableFunction,
-        error: function (e) {
-            console.log(e);
-        }
-    });
-}
-
-function clearInputSearchFunction() {
-    var clearKategoriaInput = $('#kategoria-input');
-    var clearNazwaInput = $('#nazwa-input');
-    var clearStatusSelect = $('#status-select');
-    clearKategoriaInput.val("");
-    clearNazwaInput.val("");
-    clearStatusSelect.val("");
-}
-
-
-function removeFunction() {
-    var baseLink = "http://localhost:8080/expense/delete";
-    var idParametr = $('#').val();
-    var whereParts = [];
-
-    if (idParametr) {
-        whereParts.push("/" + idParametr);
-    }
-
-    console.log(baseLink);
-
-    $.ajax({
-        url: baseLink,
-        success: removeExpenseFunction,
-        error: function (e) {
-            console.log(e);
-        }
-    });
-
-}
-$('#search-button').click(searchFunction);
-$('#clear-button').click(clearFunction);
-searchFunction();
