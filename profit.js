7309af981b2f2060670e63ba5939a7adbab9c62b
KatarzynaWojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-10-08 13:16:31 +0200

    Deleted addProfit function

--- profit.js
+++ profit.js
@@ -72,38 +72,6 @@ $(document).ready(function () {
         });
     }
 
-
-    function addFunction() {
-
-        var baseLink = "http://localhost:8080/profit/add";
-        var nazwaParametr = $('#nowy-przychod-nazwa-input').val();
-        var kwotaParametr = $('#nowy-przychod-kwota-input').val();
-        var addFunctionJson = '{"nazwa":"' + nazwaParametr + '","kwota":' + kwotaParametr + '}';
-
-        console.log(addFunctionJson);
-
-        $.ajax({
-            type: "POST",
-            url: baseLink,
-            data: addFunctionJson,
-            contentType: "application/json",
-            success: profitAddedFunction,
-            error: function (e) {
-                $('#new-profit-error-alert').show();
-                console.log(e);
-            }
-
-        })
-    }
-
-    function profitAddedFunction() {
-        var clearNazwa = $('#nowy-przychod-nazwa-input');
-        var clearKwota = $('#nowy-przychod-kwota-input');
-        clearNazwa.val("");
-        clearKwota.val("");
-        $('#new-profit-alert').show();
-    }
-
     function clearInputSearchFunction() {
         var clearNazwaInput = $('#nazwa-input');
         clearNazwaInput.val("");
@@ -148,15 +116,7 @@ $(document).ready(function () {
 
 
     })
-    $('#add-button').click(addFunction);
 
-    $('#new-profit-alert-close').click(function () {
-        $('#new-profit-alert').hide();
-    });
-
-    $('#new-profit-error-alert-close').click(() => {
-        $('#new-profit-error-alert').hide();
-    })
 
     $('#search-button').click(searchFunction);
     searchFunction()
